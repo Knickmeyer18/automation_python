e68af7e04fdd81e1b2f3bfaf029fd07f431a0d43
Knickmeyer <eknickme@cgifederal.com>
2019-01-25 14:43:57 -0500

    almost finished

--- c4automation.py
+++ c4automation.py
@@ -148,34 +148,156 @@ A COMMON PYTHON TECHNIQUE:
 
 ######################################################################################
 '''
+print()
+
+g_list = []
+
+for i in range(10):
+	g_list.append(i)
+
+print(g_list)
+for j in range(len(g_list)):
+	print("index " + str(j) + ' in g_list is: ' + str(g_list[j]))
+
+'''
+######################################################################################
+- Using range(len(g_list)) in the previously shown for loop is handy because the code
+in the lopcan access the index (as the variable j) and the value at that index as 
+(g_list[j]). Best of all, range(len(g_list)) will iterate through all the indexes of
+g_list, no matter how many items it contains.
+######################################################################################
+'''
+print()
+
+var = 2
+var2 = 11
+
+print(var in g_list)
+print(var2 in g_list)
+print('--------------------')
+print(var not in g_list)
+print(var2 not in g_list)
+
+print()
+# -----------------------------------------------
+h_list = [1,2,3]
+first, second, third = h_list
+
+print(first, second, third)
+print()
+uno, dos = 1,2
+uno, dos = dos, uno
+print(uno, dos)
+print()
+
+'''
+######################################################################################
+List Methods:
+ - index() -- provides the index of the specified value in the parenthesis
+ - append(Val) -- adds its argument to the end of the list
+ - insert( indexNum, Val) -- NEEDS two arguments... inserts the given value at that index
+ - remove()-- removes given argument from list
+ - sort() -- can take an unordered list and give it order
+######################################################################################
+'''
+i_list = []
+for j in range (10):
+	i_list.append(j)
+
+i_list.sort(reverse=True)
+print(i_list)
+'''
+ ABOUT SORT() METHOD:
+ 1)  this method sorts the list in place, don't try to capture the return value by writing
+ code like i_list=i_list.sort()
+
+ 2)  cannot sort lists that have both number values AND string values in them.
+ 
+ 3)  sort() uses "ASCIIbetical order" rather than alphabetical order for sorting strings. 
+	This means  uppercase letters come before lowercase letters. Therefore lowercase 'a'
+	comes after Uppercase 'Z'
+	SOLUTION:
+	pass str.lower for key keyword argument in the sort() method call
+	vvvvvvvvvvvvvvvvv
+'''
+letter_list =['a','z','A','Z']
+# without solution
+letter_list.sort()
+print(letter_list)
+print()
+# WITH SOLUTION
+letter_list.sort(key=str.lower)
+print(letter_list)
+
+
+# print()
+# -----------------------------------------------
+
+'''
+######################################################################################
+
+List-Like Types: Strings and Tuples
+- strings and lists are similar -- consider a string a list of text characters
+-- many of the things you can do with lists, you can do with strings (indexing, slicing, 
+using for loops, with len(), and with the 'in' and 'not in' operators. )
+--------------------------------------------------------------------------------------
+Mutable and Immutable Data Types
+- lists and Strings are different in an IMPORTANT WAY ==> 
+A list:
+	- is a mutable data type: It can have values added, removed, or changed
+a String:
+	- is immutable data type: It cannot be changed. Trying to reassign a single character
+	in a string results in a TypeError error
+
+The proper way to "mutate" a string is to use slicing and concatenation to build a new string
+by copying from parts of the old string
+ 
+
+######################################################################################
+'''
+
+
+
+print()
+# -----------------------------------------------
+
+'''
+######################################################################################
+The Tuple Data Type
+- almost identical to the list data type EXCEPT...
+	1. tuples are typed with parentheses instead of square brackets
+######################################################################################
+'''
+a_tuple = ('abc',1,2.0)
+print(a_tuple[0])
+
+print()
+
+'''
+######################################################################################
+Main difference between tuples and lists
+	2. Tuples cannot have their values modified, appended, or removed. Trying to do so
+	will cause a TypeError error
+######################################################################################
+'''
+# print()
+# -----------------------------------------------
+
+# print()
+# -----------------------------------------------
 
 # print()
 # -----------------------------------------------
 
 
 
+'''
+######################################################################################
 
+######################################################################################
+'''
+'''
+######################################################################################
 
-# print()
-# -----------------------------------------------
-
-
-
-
-
-# print()
-# -----------------------------------------------
-
-
-
-
-
-# print()
-# -----------------------------------------------
-
-# print()
-# -----------------------------------------------
-
-# print()
-# -----------------------------------------------
-
+######################################################################################
+'''
